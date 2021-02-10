c8e17b07e3bb4ee221a14186c1e6f3f16561906a
nick@deploy.at <mikowhy@tutanota.com>
2021-02-10 17:23:53 +0100

    chore: refactor

--- 3_custom_types/enums/enums.rs
+++ 3_custom_types/enums/enums.rs
@@ -44,3 +44,30 @@ fn main() {
   inspect(unload);
 }
 
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+  Add,
+  Subtract,
+}
+
+// Creates a type alias
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+fn main() {
+  // We can refer to each variant via its alias, not its long and inconvenient
+  // name.
+  let x = Operations::Add;
+}
+
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+  Add,
+  Subtract,
+}
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+  fn run(&self, x: i32, y: i32) -> i32 {
+      match self {
+          Self::Add => x + y,
+          Self::Subtract => x - y,
+      }
+  }
+}
